c779bbd0d2e3990d876089e78d3a4c3e49b29c40
Daniel Porter <dpreid@gmail.com>
2015-12-01 11:47:51 +0000

    Add legacy Jira server script

---
+++ letsencrypt-jira-legacy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+export DOMAINS="-d jira.farsetlabs.org.uk"
+export IMAGE="quay.io/letsencrypt/letsencrypt:latest"
+docker pull ${IMAGE}
+service nginx stop
+docker run -it --rm --name letsencrypt \
+    -v "/etc/letsencrypt:/etc/letsencrypt" -v "/var/lib/letsencrypt:/var/lib/letsencrypt" -p 80:80 -p 443:443 \
+    ${IMAGE} certonly \
+    --server https://acme-v01.api.letsencrypt.org/directory \
+    --agree-dev-preview ${DOMAINS} --text $@
+service nginx start

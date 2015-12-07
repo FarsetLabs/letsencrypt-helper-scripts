04463376944cd48fa930f0dd28506878bf716acb
Daniel Porter <dpreid@gmail.com>
2015-12-07 09:08:02 +0000

    Remove private beta options

--- letsencrypt-virtualmin.sh
+++ letsencrypt-virtualmin.sh
@@ -6,8 +6,8 @@ docker pull ${IMAGE}
 docker run -it --rm --name letsencrypt \
     -v "/etc/letsencrypt:/etc/letsencrypt" -v "/var/lib/letsencrypt:/var/lib/letsencrypt" -v "${ROOT}:${ROOT}" \
     ${IMAGE} certonly \
-    --server https://acme-v01.api.letsencrypt.org/directory --email "services@farsetlabs.org.uk" \
-    -a webroot --webroot-path="${ROOT}/public_html" --renew-by-default --agree-dev-preview --agree-tos -d ${DOMAIN} --text $@
+    --email "services@farsetlabs.org.uk" \
+    -a webroot --webroot-path="${ROOT}/public_html" --renew-by-default --agree-tos -d ${DOMAIN} --text $@
 ln -sf "/etc/letsencrypt/live/${DOMAIN}/cert.pem" "${ROOT}/ssl.cert"
 ln -sf "/etc/letsencrypt/live/${DOMAIN}/privkey.pem" "${ROOT}/ssl.key"
 ln -sf "/etc/letsencrypt/live/${DOMAIN}/chain.pem" "${ROOT}/ssl.ca"
